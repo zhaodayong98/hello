9413e561c7011d46cd3594673354d36d815b3e4a
zhaoyong93 <zhaoyong93@jd.com>
2023-08-26 12:24:49 +0800

    Revert "111111111111"
    This reverts commit 22df9dc7a0bad9784e4d58a13007db461a59afdf.

--- src/ListNodeq.java
+++
@@ -1,7 +0,0 @@
-public class ListNodeq {
-     public int val;
-     public ListNodeq next;
-    ListNodeq(){}
-    ListNodeq(int val){this.val=val;}
-    ListNodeq(int val,ListNodeq next){this.val=val;this.next=next;}
-}
